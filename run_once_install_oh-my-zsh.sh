053e2cef53f20277e46957e5a34d949e86ef1d94
Corbin Heist <Corbin@Corbinheist.com>
2024-12-14 19:12:08 -0800

    adding dotconfig :

--- run_once_install_oh-my-zsh.sh
+++ run_once_install_oh-my-zsh.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
+	apt-get install zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
 	echo ".oh-my-zsh already found, skipping."
